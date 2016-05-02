319272bee0ccae1628c47f795d5910a4b5c64836
xy124 <qcd@gmx.net>
2016-05-02 13:52:01 +0200

    use node script ;)

--- generateAppIcons.sh
+++ generateAppIcons.sh
@@ -9,29 +9,3 @@ bin/cordova-icon
 
 exit 0
 
-# first argument: outputpath relative to www/res/icons
-# second argument: width=height in px
-function convert {
-        inkscape -z -e www/res/icons/$1 -w $2 -h $2 app-icon.svg
-}
-
-mkdir -p www/res
-mkdir -p www/res/icons
-
-# android:
-mkdir -p www/res/icons/android
-convert android/icon-36-ldpi.png 36
-convert android/icon-48-mdpi.png 48
-convert android/icon-72-hdpi.png 72
-convert android/icon-96-xhdpi.png 96
-
-
-
-# ios:
-# TODO: double resolution for ios
-mkdir -p www/res/icons/ios
-convert ios/icon-57-2x.png 57
-convert ios/icon-57.png 57
-convert ios/icon-72-2x.png 72
-convert ios/icon-72.png 72
-
