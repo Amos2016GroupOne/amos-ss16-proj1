02ff987d11b75afe72a794d39690a59a92a2e55b
Felix Winterhalter <felix@audiofair.de>
2016-05-11 10:44:38 +0200

    Fixed devics not shoing up on rescan

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -144,7 +144,7 @@ angular.module('app.controllers', [])
         })
 
         $scope.refreshSensortags = function () {
-            $scope.devices = [];
+            $scope.devices = {};
             $scope.startScan();
         }
 
