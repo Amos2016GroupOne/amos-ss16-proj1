f9ac5263408be532556ebb99dcbcd6439ab7fe8a
Felix Winterhalter <felix@audiofair.de>
2016-05-16 11:28:42 +0200

    Copy get and set Functions to new service

--- www/js/services.js
+++ www/js/services.js
@@ -1,4 +1,26 @@
 angular.module('app', [])
-.factory("settings", [function() {
-    
-}])
+    .factory("settings", [function () {
+        function parseBool(val) { return val === true || val === "true" }
+
+        // Redundant - should access tabCtrl.getSetting
+        function getSetting(name) {
+            var ret = localStorage.getItem(name);
+            // Setze Default-Wert
+            if (ret == null) {
+                if (name == "reconnect")
+                    ret = false;
+                else if (name == "duration")
+                    ret = 5;
+
+                localStorage.setItem(name, ret);
+            }
+            if (name == "reconnect")
+                ret = parseBool(ret);
+
+            return ret;
+        }
+        
+        function setSetting(name, value) {
+            localStorage.setItem(name, value);
+        }
+    }])
