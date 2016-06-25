2b2ee644e33c6345a8bcabf13436470d47aa7b60
xy124 <qcd@gmx.net>
2016-06-25 22:25:11 +0200

    Add tour controller

---
+++ www/js/controllers/tour.js
@@ -0,0 +1,41 @@
+/*
+ * Projectname: amos-ss16-proj1
+ *
+ * Copyright (c) 2016 de.fau.cs.osr.amos2016.gruppe1
+ *
+ * This file is part of the AMOS Project 2016 @ FAU
+ * (Friedrich-Alexander University Erlangen-Nürnberg)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+// Controller for Tour
+angular.module('app.controllers')
+.controller('TourCtrl', function($scope, settings) {
+    console.log('here');
+    if (settings.getSetting('start-with-tour')) {
+        $scope.currentStep = 1;
+        settings.setSetting('start-with-tour', false);
+    } else {
+        $scope.currentStep = 2;
+    }
+
+    $scope.animation = function() {
+        // dummy to prevent errorlog.
+    };
+});
+
